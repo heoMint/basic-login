46a9018f25184863f62b553af41b9edd4fbed11d
hdwmumu <wil0110@naver.com>
2024-01-23 11:35:30 +0900

    feat : Inline style

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -27,7 +27,9 @@ export default function AuthInputs() {
           <label>Email</label>
           <input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            {/** inline style 사용 방법 */}
+            style={{background: emailNotValid ? '#fed2d2' : '#d1d5db'}}
+            // className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
